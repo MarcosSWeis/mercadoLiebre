6d05893fe1a5dcb2eb6bed82aac359b3a9ccffbd
MarcosSWeis <marcosweis01@gmail.com>
2021-11-04 20:00:34 -0300

    Procfile

--- app.js
+++ app.js
@@ -2,7 +2,10 @@ const express = require("express");
 const app = express();
 const path = require("path");
 app.use(express.static(path.join(__dirname, "/public")));
-app.listen(3000, () => console.log("El server esta funcionando"));
+
+app.listen(process.env.PORT || 3000, () =>
+  console.log("El server esta funcionando")
+);
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/views/home.html"));
 });
